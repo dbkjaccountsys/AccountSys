214c39a47c7a47b8951fc01ba318602d78c7ef8b
limst <fsbmail@126.com>
2017-09-18 10:37:18 +0800

    233223

--- src/main/java/com/dbkj/account/config/AppConfig.java
+++ src/main/java/com/dbkj/account/config/AppConfig.java
@@ -8,8 +8,12 @@ import com.dbkj.account.interceptor.LoginInterceptor;
 import com.dbkj.account.interceptor.UserAuthorityTemplateDirectiveInterceptor;
 import com.dbkj.account.model._MappingKit;
 import com.dbkj.account.service.OperaTypeService;
+import com.dbkj.account.sys.Config;
+import com.dbkj.account.sys.EmailResetCleanThread;
+import com.dbkj.account.sys.MapValueThread;
+import com.dbkj.account.sys.PayThread;
 import com.dbkj.account.sys.SysRoutes;
-import com.dbkj.account.sys.UserPhoneThread;
+import com.dbkj.account.sys.inter.LoginInter;
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -22,6 +26,7 @@ import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.plugin.ehcache.EhCachePlugin;
+import com.jfinal.plugin.redis.RedisPlugin;
 import com.jfinal.render.ViewType;
 import com.jfinal.template.Engine;
 
@@ -35,7 +40,7 @@ public class AppConfig extends JFinalConfig{
 	    //设置上传最大限制尺寸
 	    me.setMaxPostSize(1024*1024*10);
 	    me.setViewType(ViewType.FREE_MARKER);
-	    me.setErrorView(403, "/pages/other/403.html");
+	    me.setErrorView(401, "/pages/other/401.html");
 	    me.setErrorView(404, "/pages/other/404.html");
 	    me.setErrorView(500, "/pages/other/500.html");
 	}
@@ -47,10 +52,6 @@ public class AppConfig extends JFinalConfig{
 		me.add("/vertifyCode",VertifyCodeController.class);
 		me.add("/forget",ForgetPasswordController.class);
 		me.add(new ManageRoute());
-		
-		UserPhoneThread th = new UserPhoneThread();
-		Thread rth = new Thread(th);
-		rth.start();
 	}
 
 	@Override
@@ -69,6 +70,12 @@ public class AppConfig extends JFinalConfig{
 	    _MappingKit.mapping(activeRecordPlugin);
 	    
 		me.add(new EhCachePlugin());
+		
+		//配置Redis
+		RedisPlugin redis=new RedisPlugin("AccountSys",
+		                PropKit.get("redis.host","127.0.0.1"),
+		                PropKit.getInt("redis.port",6379));
+		me.add(redis);
 	}
 
 	@Override
@@ -77,6 +84,7 @@ public class AppConfig extends JFinalConfig{
 		me.add(new AuthInterceptor());
 		me.addGlobalActionInterceptor(new AdminAuthorityTemplateDirectiveInterceptor());
 		me.addGlobalActionInterceptor(new UserAuthorityTemplateDirectiveInterceptor());
+		me.add(new LoginInter());
 	}
 
 	@Override
@@ -88,6 +96,20 @@ public class AppConfig extends JFinalConfig{
 	public void afterJFinalStart() {
 		//初始化操作类型信息
 		OperaTypeService.init();
+		
+		Config.init();
+		
+		MapValueThread th1 = new MapValueThread();
+		Thread t1 = new Thread(th1);
+		t1.start();
+		
+		EmailResetCleanThread th2 = new EmailResetCleanThread();
+		Thread t2 = new Thread(th2);
+		t2.start();
+		
+		PayThread th3 = new PayThread();
+		Thread t3 = new Thread(th3);
+		t3.start();
 	}
 	
 	public static void main(String[] args) {
